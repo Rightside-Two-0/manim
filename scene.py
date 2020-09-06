afae99125a3e4363497c2e8f1a712bbaa2b7e4c9
Philip Caputo Two.0 <philip@two-0.org>
2020-09-05 18:55:24 -0700

    updating

--- scene.py
+++ scene.py
@@ -17,6 +17,46 @@ class SquareToCircle(Scene):
     def construct(self):
         for i in range(10):
             circle = Circle()
+            square = Square()
+            square.flip(RIGHT)
+            square.rotate(-3 * TAU / 8)
             circle.set_fill(PINK, opacity=0.5)
-            self.play(ShowCreation(circle))
-            self.wait(1)
+            self.wait()
+
+            self.play(ShowCreation(square))
+            self.play(Transform(square, circle))
+            self.play(FadeOut(square))
+
+class displayText(Scene):
+    def construct(self):
+        # Create TextMobjects
+        first_line = TextMobject('Create cool animations')
+        second_line = TextMobject('using Manim')
+        third_line = TextMobject('Try it out yourself.', color=RED)
+
+        # Position second line underneath first line
+        second_line.next_to(first_line, DOWN)
+
+        # Displaying text
+        self.wait(1)
+        self.play(Write(first_line), Write(second_line))
+        self.wait(1)
+        self.play(ReplacementTransform(first_line, third_line), FadeOut(second_line))
+        self.wait(2)
+
+
+class displayEquations(Scene):
+    def construct(self):
+        # Create TextMobjects
+        first_line = TextMobject('Manim also allows you')
+        second_line = TextMobject('to show beautiful math equations')
+        equation = TextMobject('$d\\left(p, q\\right)=d\\left(q, p\\right)=\\sqrt{(q_1-p_1)^2+(q_2-p_2)^2+...+(q_n-p_n)^2}=\\sqrt{\\sum_{i=1}^n\\left(q_i-p_i\\right)^2}$')
+
+        # Position second line underneath first line
+        second_line.next_to(first_line, DOWN)
+
+        # Displaying text and equation
+        self.play(Write(first_line), Write(second_line))
+        self.wait(1)
+        self.play(ReplacementTransform(first_line, equation), FadeOut(second_line))
+        self.wait(3)
